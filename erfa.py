7fe810ff2de96647b62d24301e58a5a50093db2d
nirinA raseliarison <nirina.raseliarison@gmail.com>
2014-01-10 04:34:54 +0300

    fix erfa.py

--- erfa.py
+++ erfa.py
@@ -13,9 +13,10 @@ def besselian_epoch_jd(epd):
     return _erfa.epb2jd(epd)
 
 def pmsafe(ra1, dec1, pmr1, pmd1, px1, rv1, ep1a, ep1b, ep2a, ep2b):
-    if not (type(epd) == np.ndarray):
-        raise _erfa.error('argument is not of type of ndarray')
-    if not epd.shape:
-        raise _erfa.error('argument is ndarray of length 0')
+##    for t in (ra1, dec1, pmr1, pmd1, px1, rv1, ep1a, ep1b, ep2a, ep2b):
+##        if not (type(t) == np.ndarray):
+##            raise _erfa.error('argument is not of type of ndarray')
+##        if not t.shape == ra1.shape:
+##            raise _erfa.error('argument have not the same shape')
     return _erfa.pmsafe(ra1, dec1, pmr1, pmd1, px1, rv1, ep1a, ep1b, ep2a, ep2b)
 
